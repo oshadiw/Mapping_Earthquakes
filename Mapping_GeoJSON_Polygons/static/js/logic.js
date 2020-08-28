b6cbf82e59644102afcb20d9590e48ab63a34aaf
oshadiw <oshadiw@gmail.com>
2020-08-28 18:10:06 -0500

    message

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -4,7 +4,7 @@ console.log("working");
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -27,24 +27,22 @@ let baseMaps = {
 let map = L.map('mapid', {
 	center: [43.7, -79.3],
 	zoom: 11,
-	layers: [satelliteStreets]
+	layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/oshadiw/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/oshadiw/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
     console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
-	  color: "#ffffa1",
-	  weight: 2,
-	  onEachFeature: function(feature, layer) {
-		  layer.bindPopup("<h3> Airline: " + feature.properties.airline + "<h3> <hr><h3> Destination: "
-		  + feature.properties.dst + "</h3>");
-	  }
-  }).addTo(map);
+L.geoJson(data, {
+	color: "#ffff00",
+	weight: 1,
+	onEachFeature: function(feature, layer) {
+		layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+	}
+}).addTo(map);
 });
